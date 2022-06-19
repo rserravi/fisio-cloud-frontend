de0cf43daf7ce45fddf345b8e5aeb5254800c097
rserravi <rserravi@gmail.com>
2022-06-19 15:10:07 +0200

    Added customer page and comp

--- src/components/customers-comp.js
+++ src/components/customers-comp.js
@@ -1,69 +1,184 @@
 import * as React from 'react';
 import Link from '@mui/material/Link';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
 import Title from './Title';
 import { useTranslation } from 'react-i18next';
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import { Button, IconButton, Toolbar, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
 import Stack from '@mui/material/Stack';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import PrintIcon from '@mui/icons-material/Print';
 
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
+import PrintIcon from '@mui/icons-material/Print';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import customerData from "../assets/data/dummy-data.json";
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
+import Avatar from '@mui/material/Avatar';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import LocalPrintshopIcon from '@mui/icons-material/LocalPrintshop';
+
+
+const addNewAppointment= (customerId) =>{
+  console.log("ADD APPOINTMENT IN CUSTOMER " + customerId);
 }
 
-const rows = [
-  createData(
-    0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
-  ),
-  createData(
-    1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
-  ),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
-  ),
+const editCustomer= (customerId) =>{
+  console.log("EDIT "+ customerId);
+}
+const deleteCustomer = (customerId) => {
+  console.log("DELETE CUSTOMER " + customerId);
+}
+
+const duplicateCustomer = (customerId) => {
+  console.log("DUPLICATE CUSTOMER " + customerId);
+}
+
+const printCustomer = (customerId) => {
+  console.log("PRINT CUSTOMER " + customerId);
+}
+
+const seeCustomer = (customerId) => {
+  console.log("SEE CUSTOMER " + customerId);
+}
+
+
+const RenderAppointmentButton = (props) => {
+  const {hasFocus, value } = props;
+  const _id = props.row.id;
+  const buttonElement = React.useRef(null);
+  const rippleRef = React.useRef(null);
+  const { t, i18n } = useTranslation();
+
+  React.useLayoutEffect(() => {
+    if (hasFocus) {
+      const input = buttonElement.current?.querySelector('input');
+      input?.focus();
+    } else if (rippleRef.current) {
+      // Only available in @mui/material v5.4.1 or later
+      rippleRef.current.stop({});
+    }
+  }, [hasFocus]);
+
+  return (
+     <strong>
+      {value}
+      
+   
+      <Button
+        component="button"
+        ref={buttonElement}
+        touchRippleRef={rippleRef}
+        variant="contained"
+        size="small"
+        style={{ marginLeft: 16 }}
+        // Remove button from tab sequence when cell does not have focus
+        tabIndex={hasFocus ? 0 : -1}
+        onKeyDown={(event) => {
+          if (event.key === ' ') {
+            // Prevent key navigation when focus is on button
+            event.stopPropagation();
+          }
+        }}
+     
+        onClick={(event) => {
+           addNewAppointment(_id);
+           event.stopPropagation();
+        }}
+      
+      >
+        {t("add")}
+      </Button>
+      </strong>
+  );
+};
+
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 50 },
+  { field: 'image', headerName:"Imagen", width:60, renderCell: (params)=>
+      {
+        return(
+          <>
+            <Avatar src={"./images/" + params.value} />
+          </>
+        )
+      }
+  },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 160 },
+  {
+    field: 'phoneNumber',
+    headerName: 'PhoneNumber',
+    width: 130,
+  },
+  {
+    field: 'appointments',
+    headerName: 'Appointments',
+    width: 130,
+    renderCell: RenderAppointmentButton,
+  },
+  {
+    field: 'actions',
+    type: 'actions',
+    headerName: 'Actions',
+    width: 200,
+    sortable: false,
+    getActions: (params) => [
+      <GridActionsCellItem
+      icon={<VisibilityIcon />}
+      label="See"
+      onClick={(event) => {
+        seeCustomer(params.id);
+        event.stopPropagation();
+     }}
+    />,
+      <GridActionsCellItem
+        icon={<EditIcon />}
+        label="Edit"
+        onClick={(event) => {
+          editCustomer(params.id);
+          event.stopPropagation();
+       }}
+      />,
+      <GridActionsCellItem
+      icon={<ContentCopyIcon />}
+      label="Duplicate"
+      onClick={(event) => {
+        duplicateCustomer(params.id);
+        event.stopPropagation();
+     }}
+    />,
+     <GridActionsCellItem
+        icon={<DeleteIcon />}
+        label="Delete"
+        onClick={(event) => {
+          deleteCustomer(params.id);
+          event.stopPropagation();
+       }}
+      />,
+      <GridActionsCellItem
+      icon={<LocalPrintshopIcon />}
+      label="Print"
+      onClick={(event) => {
+        printCustomer(params.id);
+        event.stopPropagation();
+     }}
+    />,
+    ],
+  },
 ];
 
+const rows = customerData.map((row) => 
+   ({
+      id: row.id, image: row.image, firstName: row.firstname, lastName: row.lastname, phoneNumber: row.phoneNumber[0].number, appointments: row.appointments.length, 
+   })
+);
 
-
-function preventDefault(event) {
-  event.preventDefault();
-}
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -105,6 +220,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
+
 export default function CustomersComponent() {
   const { t, i18n } = useTranslation();
 
@@ -120,47 +236,40 @@ export default function CustomersComponent() {
                 <SearchIcon />
             </SearchIconWrapper>
             <StyledInputBase
-                placeholder="Search…"
+                placeholder={t("search")}
                 inputProps={{ 'aria-label': 'search' }}
             />
           </Search>
+
           <Stack direction="row" spacing={2} justifyContent="flex-end">
-            <Button variant='contained' href="#">{t("addnewcustomer")}</Button>
-            <IconButton color="primary" aria-label="imprimir-formulario" component="span">
-              <PrintIcon />
-            </IconButton>
+            <Button variant='contained' href="#" startIcon={<PersonAddAlt1Icon />}>{t("addnewcustomer")} </Button>
+            <Tooltip title={t("print")}>
+              <IconButton color="primary" aria-label="imprimir-formulario" component="span">
+                <PrintIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title={t("exporttoPDF")}>
+              <IconButton color="primary" aria-label="exportar-formulario" component="span">
+                <PictureAsPdfIcon />
+              </IconButton>
+            </Tooltip>
           </Stack>
 
         </Toolbar>
       </Box>
       <Toolbar />
 
-      <Title>{t("recentorders")}</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>{t("date")}</TableCell>
-            <TableCell>{t("name")}</TableCell>
-            <TableCell>{t("shipto")}</TableCell>
-            <TableCell>{t("paymentmethod")}</TableCell>
-            <TableCell align="right">{t("saleamount")}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-        {t("seemoreorders")}
-      </Link>
+      <Title>{t("allcustomers")}</Title>
+    
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        autoHeight ="true"
+      />
+
     </React.Fragment>
   );
 }
