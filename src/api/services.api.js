0746d6c37de6d1072fac8dccc36a87e33f1b7507
rserravi <rserravi@gmail.com>
2022-07-27 18:24:58 +0200

    Added services api

---
+++ src/api/services.api.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+const rootUrl = "http://localhost:3001/v1";
+const servicesUrl = rootUrl + "/services";
+
+export const getServices= () =>{
+    return new Promise( async(resolve, reject)=>{
+        try {
+            const accessJWT = sessionStorage.getItem("accessJWT");
+            if (!accessJWT){
+                reject("Token not found!");
+            }
+            
+            const res = await axios.get(servicesUrl, {
+                headers: {
+                    Authorization :accessJWT,
+                }
+            });
+            resolve(res.data);
+            
+        } catch (error) {
+            console.log(error);
+            reject(error.message);
+        }
+    })
+}
+
+export const addServices = (frmData) =>{
+    console.log("frmdata en ADD SERVICES", frmData)
+    return new Promise( async(resolve, reject)=>{
+        try {
+            const res = await axios.post(servicesUrl, {
+                data : frmData
+            });
+            resolve(res.data);
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+export const deleteServices = (_id) =>{
+    return new Promise( async(resolve, reject)=>{
+        try {
+            const res = await axios.delete(servicesUrl, 
+                { data:{
+                    _id:_id
+                }
+            });
+            resolve(res.data);
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
--- src/components/form-components/services-comp.js
+++ src/components/form-components/services-comp.js
@@ -1,35 +1,143 @@
-import { Button, Card, Grid, IconButton, Typography } from '@mui/material';
+import { Button, Card, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, IconButton, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import * as React from 'react';
-import { getNewServicesId, getServices, getServicesRealized } from '../../utils/dataFetch-utils';
+import { getServicesRealized } from '../../utils/dataFetch-utils';
 import { useTranslation } from 'react-i18next';
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { addServices, deleteServices, getServices } from '../../api/services.api';
 
 export default function ServicesForm() {
 
     const { t } = useTranslation();
-    const servicesList = getServices();
+    const [firstLoad, setFirstLoad] = React.useState(true)
+    const [dialogOpen, setDialogOpen] = React.useState(false);
+    const [deleteDialogOpen, setDeleteDialogOpen] = React.useState(false);
+    const [deleteId, setDeleteID]= React.useState("");
 
-
-    const SeeServices = (props) =>{
-        const _id = props.currentTarget.name;
+    
+    const handleDialogClickOpen = (event) => {
         
+        setDialogOpen(true);
+      };
+    
+    const handleDialogClose = (event) => {
+        event.stopPropagation();
+        setDialogOpen(false);
+    };
+    
+    const deleteDialogClose=(event)=>{
+        event.stopPropagation();
+        setDeleteDialogOpen(false)
     }
 
-    const NewService = (event) =>{
-        console.log(event)
-        const _id = event;
-      
+    const handleDeleteDialogOpen=(event)=>{
+        event.stopPropagation();
+        setDeleteDialogOpen(true)
+    }
+
+    const initData =[
+    {
+        "_id":1,
+        "serviceName":"",
+        "priceXHour":"",
+    }
+    ]
+
+    const initNewServData = 
+        {
+            "id":"",
+            "serviceName":"",
+            "priceXHour":50,
+        }
+    
+
+    const [newServiceForSub, setNewServiceForSub]= React.useState(initNewServData);
+    const [servicesList, setServicesList] = React.useState(initData);
+
+     React.useEffect(() => {
+        if(firstLoad){
+            getServices().then(data =>{
+                setServicesList(data.result);
+                setFirstLoad(false);
+            })
+        }
+    },[firstLoad]) 
+
+   
+    const handleDialogSubmit = (event) => {
+        event.stopPropagation();
+        addServices(newServiceForSub).then(data =>{
+                    setDialogOpen(false);
+                    setFirstLoad(true)   
+            })
+        
+    };
+
+    const deleteService = (event) =>{
+        event.stopPropagation()
+        deleteServices(deleteId).then(data =>{
+            setDeleteDialogOpen(false);
+            setFirstLoad(true)
+        })
+    }
+
+    const valueOnChange = (event)=>{
+        event.stopPropagation();
+        setNewServiceForSub({...newServiceForSub,[event.target.name]:event.target.value})
     }
     
     return (
         <React.Fragment>
-           <p> {t("users")}</p>
+            <Dialog open={dialogOpen} onClose={handleDialogClose}>
+                <DialogTitle>{t("addservice")}</DialogTitle>
+                <DialogContent>
+                <DialogContentText>
+                    {t("defineService")}
+                </DialogContentText>
+                <TextField
+                    margin="dense"
+                    id="serviceName"
+                    name="serviceName"
+                    label={t("serviceName")}
+                    onChange={valueOnChange}
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    focused
+                />
+                <TextField
+                    margin="dense"
+                    id="priceXHour"
+                    name="priceXHour"
+                    label={t("priceXHour")}
+                    onChange={valueOnChange}
+                    type="number"
+                    fullWidth
+                    variant="standard"
+                    focused
+                />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleDialogClose}>{t("cancel")}</Button>
+                    <Button onClick={handleDialogSubmit}>{t("add")}</Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog open={deleteDialogOpen} onClose={deleteDialogClose}>
+                <DialogTitle>{t("deleteservice")}</DialogTitle>
+                <DialogContent>
+                <DialogContentText>
+                    {t("areyousureyouwanttodelete")}
+                </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={deleteDialogClose}>{t("cancel")}</Button>
+                    <Button onClick={deleteService}>{t("delete")}</Button>
+                </DialogActions>
+            </Dialog>
+           <h3> {t("users")}</h3>
         <Button onClick ={(e)=>{
             e.stopPropagation();
-            NewService(getNewServicesId())}}>{t("addservice")
-        }
+            handleDialogClickOpen(e)}}>{t("addservice")}
         </Button>
         <Box >       
             <Grid container spacing={2} rowSpacing={2} justifyContent="flex-start" alignItems="flex-start">
@@ -39,21 +147,19 @@ export default function ServicesForm() {
                 
                    <Card key={id} sx={{ display: 'flex', width: '100%', mb: 2 }}>
                       <Grid container  direction="row" justifyContent="flex-start" alignItems="center">
-                           <Typography variant="h6" align='left' sx={{ ml: 2, width: 65}}>{t("id")}: {service.id.toString()}.</Typography>
+                           <Typography variant="h6" align='left' sx={{ ml: 2, width: 330}}>{t("id")}: {service._id.toString()}.</Typography>
                            <Typography variant="h6" align='left' sx={{ ml: 2, width:280}}>{t("service")}:<b> {service.serviceName}</b>.</Typography>
                            <Typography variant="h6" align='left' sx={{ ml: 2 }}>{t("orientativeprice")}: {service.priceXHour} €</Typography>
-                           <IconButton name={service.id} sx={{ ml: 4 }} onClick={SeeServices}>
-                               <RemoveRedEyeIcon />
-                           </IconButton>
-                           <IconButton sx={{ mr: 2 }}>
+                           <IconButton sx={{ ml: 4 }} onClick={(event)=>{
+                                setDeleteID(service._id); 
+                                handleDeleteDialogOpen(event)
+                            }}>
                                <DeleteForeverIcon />
                            </IconButton>
                            <Typography variant="h6" align='left' sx={{ ml: 2, width: 300 }}> {t("performed")}: {getServicesRealized(service.id)} </Typography>
                        </Grid>
                    </Card>
                ))}
-               
-                    
                 </Grid>
             </Grid>
         </Box>
